edab1547c7ae6f0c0c3c303aa46c68654b38a102
hyamakawa <h-yamakawa@esm.co.jp>
2023-02-17 18:25:35 +0900

    プロジェクト土台を作った

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app id="app-root">
+    <v-app-bar
+      app
+      dark
+      color="primary"
+    >
+      <v-toolbar-title>
+        メモ帳アプリ
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <memo-app />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+import MemoApp from '@/view/MemoApp'
+
+export default Vue.extend({
+  components: {
+    MemoApp
+  },
+  data () {
+    return {}
+  },
+  created () {}
+})
+</script>
---
+++ src/view/MemoApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    メモ帳アプリだよ
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {},
+  computed: {},
+  methods: {}
+}
+</script>
