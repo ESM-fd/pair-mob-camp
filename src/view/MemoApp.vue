f737841b394c0b652c3890a6593afbd8fad598d6
ma-miyagawa <miyagawa@esm.co.jp>
2023-03-07 11:52:29 +0900

    メモ入力画面初回

--- src/view/MemoApp.vue
+++ src/view/MemoApp.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    メモ帳アプリだよ
+    <v-btn
+      color="blue"
+      dark
+      >
+      完了
+    </v-btn>
+    <v-row v-for="memo in memoList" :key="memo.id">
+      <v-text-field
+        label="題目"
+        type="input"
+      ></v-text-field>
+      <v-textarea
+        label="メモ内容"
+        name="input-7-1"
+      ></v-textarea>
+    </v-row>
+    <v-btn
+      dark
+      small
+      color="grey"
+      class="ma-2"
+      @click="addInput()"
+    >
+      <v-icon dark>mdi-plus</v-icon>
+    </v-btn>
   </div>
 </template>
 
